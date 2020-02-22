bb8a13bee78850693c97ebae02bd7e5edf9213fe
mjrb <www.w.com123@gmail.com>
2020-02-22 16:08:55 -0500

    init

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject herokru "0.1.0-SNAPSHOT"
+  :description "stupid heroku clone"
+  :url "http://example.com/FIXME"
+  :license {:name "Gnu Affero General Public License"
+            :url "http://gnu.org/licenses/agpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+  		 [clj-http "3.10.0"]
+                 [cheshire "5.6.1"]
+                 [slingshot "0.12.2"]
+                 [com.novemberain/monger "3.1.0"]
+                 [lispyclouds/clj-docker-client "0.5.1"]])
---
+++ test/herokru/core_test.clj
@@ -0,0 +1,7 @@
+(ns herokru.core-test
+  (:require [clojure.test :refer :all]
+            [herokru.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
